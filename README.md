f036263266f08fe837227119d1b9a0a042888a2d
EricLi404 <leiflyy@outlook.com>
2021-10-27 20:01:25 +0800

    ref scrape_url

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # rusting
+
+
+
+## scrape_url
+
+### run
+```shell
+# 在 项目根目录 执行
+# -p 指定要运行的 create
+cargo run -p scrape_url -- https://blog.rust-lang.org/  ./a.md
+```
--- scrape_url/src/main.rs
+++ scrape_url/src/main.rs
@@ -1,15 +1,24 @@
 use std::fs;
 
-fn main() {
-    let url = "https://www.rust-lang.org/";
-    let output = "rust.md";
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut url = "https://www.rust-lang.org/";
+    let mut output = "./target/rust.md";
 
-    println!("Fetching url: {}", url);
-    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let args = std::env::args().collect::<Vec<String>>();
+    if let [_path, _url, _output, ..] = args.as_slice() {
+        output = _output;
+        url = _url;
+    } else {
+        eprintln!("参数缺失");
+    }
+    println!("Fetching url: {}, output: {}", url, output);
+    let body = reqwest::blocking::get(url)?.text()?;
 
     println!("Converting html to markdown...");
     let md = html2md::parse_html(&body);
 
-    fs::write(output, md.as_bytes()).unwrap();
+    fs::write(output, md.as_bytes())?;
     println!("Converted markdown has been saved in {}.", output);
+
+    Ok(())
 }
